94804550f0adda9398e97480c847c5512ed7444d
Pushsec - Rafał Dobrosielski <70483037+pushsec@users.noreply.github.com>
2021-06-17 09:47:44 +0200

    Task Scheduler to Create Restore Point
    Create Task in Task Scheduler to Create Restore Point script to intune

---
+++ Restore point.ps1
@@ -0,0 +1,7 @@
+﻿$A = New-ScheduledTaskAction -Execute "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" -Argument '-ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description \"My Restore Point Startup\" -RestorePointType \"MODIFY_SETTINGS\"" '
+$T = New-ScheduledTaskTrigger -Weekly -AT "12:00" -DaysOfWee Wednesday
+$P = New-ScheduledTaskPrincipal "NT AUTHORITY\SYSTEM"  -RunLevel Highest
+$S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+$D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+Register-ScheduledTask Weekly_Restore_Point -InputObject $D
+
